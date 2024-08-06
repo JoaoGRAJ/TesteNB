53f23ff39a89af727824a2e4b63811bf482c11bb
JoaoGRAJ <guijo959@gmail.com>
2024-08-06 14:00:57 -0300

    Add files via upload

--- ApplicationAreaTrapezio/src/applicationareatrapezio/ApplicationAreaTrapezio.java
+++ ApplicationAreaTrapezio/src/applicationareatrapezio/ApplicationAreaTrapezio.java
@@ -7,15 +7,15 @@ public class ApplicationAreaTrapezio {
 
     public static void main(String[] args) {
         Scanner ler = new Scanner (System.in);
-        double bMaior, bMenor, altura, areaT;
-        System.out.println("Digite o valor da Base Maior: ");
-        bMaior = ler.nextDouble();
+        double bMenor, bMaior, Altura, areaT;
         System.out.println("Digite o valor da Base Menor: ");
         bMenor = ler.nextDouble();
+        System.out.println("Digite o valor da Base Maior: ");
+        bMaior = ler.nextDouble();
         System.out.println("Digite o valor da Altura: ");
-        altura = ler.nextDouble();
+        Altura = ler.nextDouble();
         
-        areaT = (bMaior + bMenor) * altura / 2.0;
+        areaT = (bMaior + bMenor) * Altura / 2.0;
         System.out.println("Area do trapezio = %.2f", areaT);
         
         ler.close();
